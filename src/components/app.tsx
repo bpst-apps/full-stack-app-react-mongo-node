e0637c1dab0b90a5fd6f6ac0bbcf8f5ff840c99a
A6218164 <a6218164@A6218164s-MacBook-Pro.local>
2023-01-20 14:41:30 +0530

    counter implementation & react DOM visualization

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,8 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./header";
 
 const App = () => {
     const [counter, setCounter] = useState(0)
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCounter(counter + 1)
+        }, 1000)
+
+        return () => {
+            clearInterval(intervalId)
+        }
+    });
+
     return (
         <div className="container">
             < Header message="Naming Constents"/>
